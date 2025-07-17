daec9608fd0380dc69bd3f9cf29b1e2c720fc24d
Jacques Hamel <jacques.hamel@ibm.com>
2025-07-17 11:08:41 -0400

    Premier commit du projet ConsoleApp1

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
+Console.WriteLine("Hello, World!");
+clsMaClasse macls = new clsMaClasse();
+macls.AfficherMessage();
+Console.ReadKey();
---
+++ clsMaClasse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class clsMaClasse
+    {
+        private string message;
+        // Constructeur
+        public clsMaClasse()
+        {
+            message = string.Empty;
+            message = "MaClasse est initialisée";
+        }
+
+        // Méthode pour afficher le message
+        public void AfficherMessage()
+        {
+            Console.WriteLine("Message : " + message);
+        }
+    }
+}
