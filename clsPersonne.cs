1e980e469ea4aa8a3291e1597fb0610fe889ca29
Jacques Hamel <jacques.hamel@ibm.com>
2025-07-22 10:14:58 -0400

    Ajouté personne

---
+++ clsPersonne.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class clsPersonne
+    {
+        private string nom;
+        private string prenom;
+        private string ville;
+        public clsPersonne()
+        {
+            this.nom = string.Empty;
+            this.prenom = string.Empty;
+            this.ville = string.Empty;
+        }
+    }
+}
